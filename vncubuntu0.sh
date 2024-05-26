4436afda885017d5f4e5197f3c641319516b7bae
Lucas <lucascontatos99@gmail.com>
2024-05-25 21:58:36 -0300

    Add files via upload

---
+++ vncubuntu0.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "
+
+#########################################
+#
+#
+#	SCRIPT DE INSTALAÇÃO VNC BY
+#
+#	 Ａｌｌｇｅｎｉｕｓ
+#
+#
+#########################################
+"
+
+sleep 7
+
+# Atualizar o sistema
+
+sudo apt update
+
+# Instalar x11vnc
+
+sudo apt install x11vnc
+
+# Criar o arquivo de serviço para x11vnc
+
+echo "[Unit]
+Description=x11vnc service
+After=display-manager.service network.target syslog.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd abc,123
+ExecStop=/usr/bin/killall x11vnc
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /lib/systemd/system/x11vnc.service
+
+sleep 2
+
+sudo systemctl daemon-reload
+sudo systemctl restart x11vnc.service
+sudo systemctl stop x11vnc.service
+sudo systemctl enable x11vnc.service
+sudo systemctl start x11vnc.service
+
+sleep 2
+
+# Reiniciar
+
+echo "⚠ EM 10 SEGUNDOS O PDV LINUX VAI REINICIAR PARA APLICAR TODA INSTALAÇÃO ⚠"
+
+sleep 10
+
+sudo systemctl reboot
