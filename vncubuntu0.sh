bcf39d6b6498828ddb7d5005bf183b50d7d62f4c
Lucas <lucascontatos99@gmail.com>
2024-05-27 17:53:54 -0300

    vncubuntu0.sh

--- vncubuntu0.sh
+++ vncubuntu0.sh
@@ -42,7 +42,7 @@ sudo systemctl start x11vnc.service
 
 # Reiniciar
 
-echo "⚠ EM 10 SEGUNDOS O PDV LINUX VAI REINICIAR PARA APLICAR TODA INSTALAÇÃO ⚠"
+echo "EM 10 SEGUNDOS O PDV LINUX VAI REINICIAR PARA APLICAR TODA INSTALAÇÃO!!!"
 
 sleep 10
 
