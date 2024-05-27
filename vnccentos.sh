faf7c23acdd5f44d54d4b25016ad09b1bf5078b7
Lucas <lucascontatos99@gmail.com>
2024-05-27 13:52:06 -0300

    Add files via upload

---
+++ vnccentos.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "
+#########################################
+#
+#
+#	SCRIPT DE INSTALAÇÃO VNC BY
+#
+#	 Ａｌｌｇｅｎｉｕｓ
+#
+#
+#########################################"
+
+sleep 5
+
+# Atualizar o sistema
+
+sudo yum update
+
+# Instalar wget
+
+sudo yum install wget
+
+# Instalar x11vnc
+
+sudo yum install x11vnc
+
+# Criar o arquivo de serviço para x11vnc
+
+echo '[Unit]
+Description=x11vnc service
+After=display-manager.service network.target syslog.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd abc,123
+ExecStop=/usr/bin/killall x11vnc
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target' | sudo tee /lib/systemd/system/x11vnc.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable x11vnc.service
+sudo systemctl start x11vnc.service
+
+# Reiniciar
+
+echo "EM 10 SEGUNDOS O PDV LINUX VAI REINICIAR PARA APLICAR TODA INSTALAÇÃO!!!"
+
+sleep 10
+
+sudo systemctl reboot
